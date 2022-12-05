7df41a38106536fa46c2abd7c172c0a44e8f0c79
2603003199a <58811224+skybird23333@users.noreply.github.com>
2022-12-05 13:15:43 +0800

    Feat: Horizontal graphs and and calculate average

--- embedded.js
+++ embedded.js
@@ -1,8 +1,28 @@
 //wrap the series calculation function so that we can get the data
 const _calcfunc = $.fn.reportCardBoxplotCalculateAllSeriesData
-    
+
+/**
+ * @typedef IReportCardSeriesData
+ * @property {number[][]} allGraphData
+ * @property {[]} allOutliers
+ * @property {{info: null, uid: null, value: number}[][]} input
+ */
+
 $.fn.reportCardBoxplotCalculateAllSeriesData = function () {
+    /**
+     * @type {IReportCardSeriesData}
+     */
     const result = _calcfunc.apply(this, arguments)
+    //OPTION: Calculate average
+    const averageScores = []
+    for(let i = 0; i<=4; i++) {//for each of 5 figure summary
+        let totalScores = 0
+        result.allGraphData.forEach((assessment) => {
+            totalScores += assessment[i]
+        })
+        averageScores.push(totalScores / result.allGraphData.length)
+    }
+    result.allGraphData.push(averageScores)
     handleResultsData(result)
     return result
 }
@@ -12,6 +32,17 @@ const _renderfunc = $.fn.reportCardBoxplotRender
 
 $.fn.reportCardBoxplotRender = function () {
     renderResultsData()
+    //OPTION: Horizontal graphs
+    //5 Figure summary depends on this option
+    arguments[0].orientation = 'horizontal'
+
+    //OPTION: Calculate average
+    arguments[0].allAssessments.push('Average') //Push a new assessment named Average
+    arguments[0].additionalSeries[0].data.push(
+        arguments[0].additionalSeries[0].data.reduce((partialSum, a) => partialSum + a, 0) / arguments[0].additionalSeries[0].data.filter(i => !!i).length
+    )
+
+
     return _renderfunc.apply(this, arguments)
 }
 
@@ -40,7 +71,6 @@ async function renderResultsData() {
     assessmentData.forEach((assessment, assessmentIndex) => {
         
         assessment.forEach((number, numberIndex) => {
-            if(numberIndex > 0) return
             //creating the spacing element(inside the loop because apparently you need a new instance or smth)
             const spacingElm = document.createElement('tspan')
             spacingElm.setAttribute('dy', '14')
@@ -48,7 +78,7 @@ async function renderResultsData() {
             spacingElm.setAttribute('style', 'transform-origin: 0px 0px; white-space: inherit;')
             spacingElm.innerText = '​'
             //creating the text element
-            const textElm = document.createTextNode(`\n${foo[numberIndex]}: ${Math.round(number)}`)
+            const textElm = document.createTextNode(`|${Math.round(number)}`)
             //insent the element
             elm.children[assessmentIndex].appendChild(spacingElm)
             elm.children[assessmentIndex].appendChild(textElm)
